733ff928e778bcc4f0b4ab78b7fdd3f00b267567
moein <moein.shariatnia@gmail.com>
2024-03-30 21:18:21 +0330

    added prompts module

---
+++ utils/prompts.py
@@ -0,0 +1,14 @@
+"""
+Contains prompt templates for different tasks.
+"""
+
+VANILLA_QA_PROMPT_MISTRAL = """
+### [INST]
+Instruction: Answer the question based on your knowledge
+
+### QUESTION:
+{question}
+
+[/INST]
+ """
+
