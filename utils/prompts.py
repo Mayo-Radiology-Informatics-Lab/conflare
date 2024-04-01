ce14ad9d1f0ba8cbdc43680f1465544095ce172d
moein <moein.shariatnia@gmail.com>
2024-04-01 09:32:17 +0330

    added simple rag pipeline

--- utils/prompts.py
+++ utils/prompts.py
@@ -11,13 +11,29 @@ Instruction: Answer the question based on your knowledge
 
  """
 
-VANILLA_QA_PROMPT_MISTRAL = """
-### [INST]
-Instruction: Answer the question based on your knowledge
+#####
+# To be used for MISTRAL model
+#####
+
+# VANILLA_QA_PROMPT = """
+# ### [INST]
+# Instruction: Answer the question based on your knowledge
+
+# ### QUESTION:
+# {question}
+
+# [/INST]
+#  """
+
+# Note to add the specialized chars like [INST], [/INST] if using with a MISTRAL model
+RAG_QA_PROMPT = """
+###
+Instruction: Answer the question based on the provided context. Only rely on
+the context to build your answer and do not use your own knowledge:
+
+{context}
 
 ### QUESTION:
 {question}
 
-[/INST]
- """
-
+"""
